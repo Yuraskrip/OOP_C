81ba76f2c9c47c6fddc9908c5f411f54c63d9157
Yuraskrip <kozakskrip@gmail.com>
2022-07-06 21:07:20 +0300

    Созданы классы, добавлены диаграммы

---
+++ OOP_C/Departament.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_C
+{
+    public class Departament : Production
+    {
+        public string nameofdepartament { get; set; }
+
+        public string appointment { get; set; }
+    }
+}
---
+++ OOP_C/uchastok.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_C
+{
+    public class Uchastok : Departament
+    {
+        public string nameUchastok { get; set; }
+
+        public string appointmentUchastok { get; set; }
+    }
+}
