cbcc3d5dcbd4124c3d5e8fb67b14d4a214630d28
Yuraskrip <kozakskrip@gmail.com>
2022-07-07 16:10:14 +0300

    Выполнено первое задание. Создан класс с закрытыми полями, объект класса и вывод информации в консоль

---
+++ OOP_C/Bank.cs
@@ -0,0 +1,42 @@
+﻿
+namespace OOP_C
+{
+
+    /// <summary>
+    /// Перечисляемый тип банковского счета
+    /// </summary>
+    enum CheckType
+    {
+        Current,
+        Savings
+    }
+    internal class Bank
+    {
+        /// <summary>
+        /// Создание класса объекта с закрытыми полями
+        /// </summary>
+        /// <param name="checkNumber">Номер счета</param>
+        /// <param name="balans">Баланс</param>
+        /// <param name="type">Тип банковского счета</param>
+        private Bank(string accountNumber, decimal balans, CheckType type)
+        {
+            CheckNumber = accountNumber;
+            Balans = balans;
+            Type = type;
+            
+        }
+        public string CheckNumber { get; set; }
+        public decimal Balans { get; set; }
+        public CheckType Type { get; set; }
+       public override string ToString() => $"CheckNumber - {CheckNumber}, Balans - {Balans}, Type - {Type}";
+
+
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank("1", 80000000, CheckType.Savings);
+            Console.WriteLine(bank);
+            Console.ReadLine(); 
+        }
+
+    }
+}
