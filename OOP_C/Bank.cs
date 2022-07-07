7122827a0eba40edb086466516058cc20d160a02
Yuraskrip <kozakskrip@gmail.com>
2022-07-07 22:08:35 +0300

    Выполнено задание 2. Создал статическую переменную и метод, увеличивающий номер счета

--- OOP_C/Bank.cs
+++ OOP_C/Bank.cs
@@ -18,9 +18,9 @@ namespace OOP_C
         /// <param name="checkNumber">Номер счета</param>
         /// <param name="balans">Баланс</param>
         /// <param name="type">Тип банковского счета</param>
-        private Bank(string accountNumber, decimal balans, CheckType type)
+        private Bank(string checkNumber, decimal balans, CheckType type)
         {
-            CheckNumber = accountNumber;
+            CheckNumber = GetNextId().ToString();
             Balans = balans;
             Type = type;
             
@@ -28,13 +28,33 @@ namespace OOP_C
         public string CheckNumber { get; set; }
         public decimal Balans { get; set; }
         public CheckType Type { get; set; }
-       public override string ToString() => $"CheckNumber - {CheckNumber}, Balans - {Balans}, Type - {Type}";
+
+
+        public static int number = 1;
+        /// <summary>
+        /// Увеличение значения переменной
+        /// </summary>
+        /// <returns></returns>
+        public int GetNextId()
+        {
+            return number++;
+        }
+
+       
+        public override string ToString() => $"CheckNumber - {CheckNumber}, Balans - {Balans}, Type - {Type}";
 
 
         static void Main(string[] args)
         {
-            Bank bank = new Bank("1", 80000000, CheckType.Savings);
+          
+            Bank bank = new Bank("ы", 80000000, CheckType.Savings);
             Console.WriteLine(bank);
+             bank = new Bank("ы", 30000000, CheckType.Current);
+            Console.WriteLine(bank);
+             bank = new Bank("ы", 25000000, CheckType.Savings);
+            
+            Console.WriteLine(bank);
+
             Console.ReadLine(); 
         }
 
